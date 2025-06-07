5063605087985a9f79444802b57c92f6e56a2470
jcarabantes <jcarabantes@github.com>
2025-06-07 21:42:46 +0200

    Output through tabulate, moved class outside and minor changes

--- s0ur.py
+++ s0ur.py
@@ -1,13 +1,6 @@
-from pprint import pprint
 from impacket.ldap import ldap
-from impacket.ldap.ldaptypes import LDAP_SID
-from impacket.ldap.ldapasn1 import SearchResultEntry #usefull for handling the ASN1 format  
-from impacket.dcerpc.v5 import samr
-from impacket.dcerpc.v5.samr import SAMPR_USER_INFO_BUFFER
-from impacket.dcerpc.v5 import transport
-from datetime import datetime
+from simpleadusers import SimpleADUsers
 from functions import *
-import logging
 import sys
 import time
 import getpass
@@ -20,225 +13,24 @@ import getpass
 # Deploy-ComputerDeception
 # Deploy-PrivilegedUserDeception
 # check for Protection DenyLogon property (https://github.com/samratashok/Deploy-Deception/blob/master/Deploy-Deception.ps1#L779)
-# improve output
 # add --query all or specific ldap query (such as just non-empty descriptions, etc.)
 # Test attrs: ldapsearch -x -b  dc=domain,dc=local -H ldap://192.168.1.10 -D "CN=M RS,CN=Users,DC=evilcorp,DC=local" -W
 # Improve debug output too
 
-# Simplified class from GetADUsers.py
-class SimpleADUsers:
-    def __init__(self, username, password, domain, dc_ip):
-        self.username = username
-        self.password = password
-        self.domain = domain
-        self.dc_ip = dc_ip
-
-        self.baseDN = ','.join([f'dc={part}' for part in domain.replace('DC=', '').split(',')])
-        # self.header = ["sAMAccountName", "description", "lastLogon", "userAccountControl"]
-        self.properties = ["sAMAccountName", "pwdLastSet", "lastLogon", "description"]
-       
-        self.colLen = [20, 30, 19, 19]
-        self.outputFormat = ' '.join(['{%d:%ds}' % (i, w) for i, w in enumerate(self.colLen)])
-
-    def _getUnixTime(self, t):
-        t -= 116444736000000000
-        t //= 10000000
-        return t
-
-    def _changeGeneralizedTime(self, gt):
-        
-        # whenCreated and other fields return values like: whenCreated: 20250320064604.0Z and whenChanged: 20250320064749.0Z
-        # remove 0Z from the LDAP output
-        noZ = gt.split('.')[0]
-        formated_date = datetime.strptime(noZ, "%Y%m%d%H%M%S")
-        return formated_date.strftime("%Y-%m-%d %H:%M:%S")
-
-    def _processRecord(self, item):
-        if not isinstance(item, SearchResultEntry):
-            return
-
-        sAMAccountName = ''
-        pwdLastSet = ''
-        mail = ''
-        lastLogon = 'N/A'
-        whenCreated = 'N/A'
-
-        if debug: print(item['attributes'])
-
-        try:
-            for attribute in item['attributes']:
-                attr_type = str(attribute['type'])
-                if attr_type == 'sAMAccountName':
-                    name = attribute['vals'][0].asOctets().decode('utf-8')
-                    if not name.endswith('$'):
-                        sAMAccountName = name
-                elif attr_type == 'pwdLastSet':
-                    val = int(str(attribute['vals'][0]))
-                    pwdLastSet = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
-                elif attr_type == 'lastLogon':
-                    val = int(str(attribute['vals'][0]))
-                    lastLogon = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
-                # elif attr_type == 'mail':
-                #     mail = str(attribute['vals'][0])
-                elif attr_type == 'whenCreated':
-                    val = str(attribute['vals'][0])
-                    whenCreated = str(self._changeGeneralizedTime(val))
-                   # description = str(attribute['vals'][0])
-                elif attr_type == 'description':
-                    description = str(attribute['vals'][0])
-
-            if sAMAccountName:
-                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
-                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
-                print(self.outputFormat.format(sAMAccountName, pwdLastSet, lastLogon, whenCreated))
-
-        except Exception as e:
-            logging.debug("Exception", exc_info=True)
-            logging.error(f"Error processing record: {e}")
-
-    def processDescription(self, item):
-        if not isinstance(item, SearchResultEntry):
-            return
-
-        sAMAccountName = ''
-        description = None
-
-        if debug: print("processDescription", item['attributes'])
-
-        try:
-            for attribute in item['attributes']:
-                attr_type = str(attribute['type'])
-                if attr_type == 'sAMAccountName':
-                    name = attribute['vals'][0].asOctets().decode('utf-8')
-                    if not name.endswith('$'):
-                        sAMAccountName = name
-                elif attr_type == 'description':
-                    # val = 
-                    description = str(attribute['vals'][0])
-
-            if sAMAccountName and description:
-                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
-                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
-                print((sAMAccountName, description))
-
-        except Exception as e:
-            logging.debug("Exception", exc_info=True)
-            logging.error(f"Error processing record: {e}")
-
-    def processLastLogon(self, item):
-        if not isinstance(item, SearchResultEntry):
-            return
-
-        sAMAccountName = ''
-        lastLogon = None
-
-        if debug: print("processLastLogon", item['attributes'])
-
-        try:
-            for attribute in item['attributes']:
-                attr_type = str(attribute['type'])
-                if attr_type == 'sAMAccountName':
-                    name = attribute['vals'][0].asOctets().decode('utf-8')
-                    if not name.endswith('$'):
-                        sAMAccountName = name
-                elif attr_type == 'lastLogon':
-                    val = int(str(attribute['vals'][0]))
-                    lastLogon = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
-
-            if sAMAccountName and lastLogon == "<never>":
-                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
-                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
-                print((sAMAccountName, lastLogon))
-
-        except Exception as e:
-            logging.debug("Exception", exc_info=True)
-            logging.error(f"Error processing record: {e}")
-
-    def processWhenCreated(self, item):
-        if not isinstance(item, SearchResultEntry):
-            return
-
-        sAMAccountName = ''
-        lastLogon = None
-
-        if debug: print("processWhenCreated", item['attributes'])
-
-        try:
-            for attribute in item['attributes']:
-                attr_type = str(attribute['type'])
-                if attr_type == 'sAMAccountName':
-                    name = attribute['vals'][0].asOctets().decode('utf-8')
-                    if not name.endswith('$'):
-                        sAMAccountName = name
-                elif attr_type == 'whenCreated':
-                    val = str(attribute['vals'][0])
-                    whenCreated = str(self._changeGeneralizedTime(val))
-
-            if sAMAccountName and whenCreated:
-                print((sAMAccountName, whenCreated))
-
-        except Exception as e:
-            logging.debug("Exception", exc_info=True)
-            logging.error(f"Error processing record: {e}")
-
-    def fetch_non_empty_descriptions(self, ldap_conn, search_filter):
-        print("Showing users' description if there's any passwords, care.")
-        sc = ldap.SimplePagedResultsControl(size=100)
-        ldap_conn.search(searchFilter=search_filter,
-                            attributes=["sAMAccountName", "description"],
-                            searchControls=[sc],
-                            perRecordCallback=self.processDescription)
-           
-    def never_logged(self, ldap_conn, search_filter):
-        print("Showing users that have no logon date")
-        sc = ldap.SimplePagedResultsControl(size=100)
-        ldap_conn.search(searchFilter=search_filter,
-                             attributes=["sAMAccountName", "lastLogon"],
-                             searchControls=[sc],
-                             perRecordCallback=self.processLastLogon)
-
-    def recent_users(self, ldap_conn, search_filter):
-        print("Showing creation date for each user")
-        sc = ldap.SimplePagedResultsControl(size=100)
-        ldap_conn.search(searchFilter=search_filter,
-                             attributes=["sAMAccountName", "whenCreated"],
-                             searchControls=[sc],
-                             perRecordCallback=self.processWhenCreated)
-
-    def run(self):
-        print(self.outputFormat.format(*self.properties))
-        print('  '.join(['-' * l for l in self.colLen]))
-        properties = ["sAMAccountName", "pwdLastSet", "lastLogon", "whenCreated"]
-        #properties = ["sAMAccountName", "pwdLastSet", "lastLogon", "description"]
-        try:
-            ldap_conn = ldap.LDAPConnection(f'ldap://{self.dc_ip}', self.baseDN)
-            ldap_conn.login(self.username, self.password, self.domain.split(',')[0].split('=')[1])
-
-            search_filter = "(&(objectCategory=person)(objectClass=user))"
-            sc = ldap.SimplePagedResultsControl(size=100)
-            ldap_conn.search(searchFilter=search_filter,
-                             attributes=properties,
-                             searchControls=[sc],
-                             perRecordCallback=self._processRecord)
-
-            ldap_conn.close()
-
-        except ldap.LDAPSearchError as e:
-            logging.error(f"LDAP search failed: {e}")
-
-# s0ur.py ldap -dc-ip -d "DC=evilcorp,DC=local" -u username -p password
-# s0ur.py adws -dc-ip -d "DC=evilcorp,DC=local" -u username -p password
-
 def main():
+
+    # s0ur.py ldap -dc-ip -d "DC=evilcorp,DC=local" -u username -p password
+    # s0ur.py adws -dc-ip -d "DC=evilcorp,DC=local" -u username -p password
+
     global debug
     args = parse_args()
-    debug = args.debug
+    #debug = args.debug
 
     if not args.password:
         args.password = getpass.getpass("Password: ")
 
     if args.mode == "ldap":
-        ad = SimpleADUsers(args.username, args.password, args.domain, args.dc_ip)
+        ad = SimpleADUsers(args.username, args.password, args.domain, args.dc_ip, args.debug)
 
         try:
             ldap_conn = ldap.LDAPConnection(f'ldap://{ad.dc_ip}', ad.baseDN)
@@ -254,8 +46,6 @@ def main():
 
             # recent users creation
             ad.recent_users(ldap_conn, search_filter)
-            
-            # ad.run()
 
             ldap_conn.close()
         except ldap.LDAPSearchError as e:
---
+++ simpleadusers.py
@@ -0,0 +1,205 @@
+from impacket.ldap import ldap
+from impacket.ldap.ldaptypes import LDAP_SID
+from impacket.ldap.ldapasn1 import SearchResultEntry #usefull for handling the ASN1 format  
+from impacket.dcerpc.v5 import samr
+from impacket.dcerpc.v5.samr import SAMPR_USER_INFO_BUFFER
+from impacket.dcerpc.v5 import transport
+from tabulate import tabulate
+from datetime import datetime
+import logging
+
+# Simplified class from GetADUsers.py
+class SimpleADUsers:
+    
+    def __init__(self, username, password, domain, dc_ip, debug):
+        self.username = username
+        self.password = password
+        self.domain = domain
+        self.dc_ip = dc_ip
+        self.debug = debug
+
+        self.baseDN = ','.join([f'dc={part}' for part in domain.replace('DC=', '').split(',')])
+        
+        self.attributes_list = [
+            ["sAMAccountName", "description"],
+            ["sAMAccountName", "lastLogon"],
+            ["sAMAccountName", "whenCreated"]
+        ]
+        self.rows = [] # this will be used with tabulate when printing results
+        
+    def _getUnixTime(self, t):
+        t -= 116444736000000000
+        t //= 10000000
+        return t
+
+    def _changeGeneralizedTime(self, gt):
+        
+        # whenCreated and other fields return values like: whenCreated: 20250320064604.0Z and whenChanged: 20250320064749.0Z
+        # remove 0Z from the LDAP output
+        noZ = gt.split('.')[0]
+        formated_date = datetime.strptime(noZ, "%Y%m%d%H%M%S")
+        return formated_date.strftime("%Y-%m-%d %H:%M:%S")
+
+    def _processRecord(self, item):
+        if not isinstance(item, SearchResultEntry):
+            return
+
+        sAMAccountName = ''
+        pwdLastSet = ''
+        mail = ''
+        lastLogon = 'N/A'
+        whenCreated = 'N/A'
+
+        if self.debug: print(item['attributes'])
+
+        try:
+            for attribute in item['attributes']:
+                attr_type = str(attribute['type'])
+                if attr_type == 'sAMAccountName':
+                    name = attribute['vals'][0].asOctets().decode('utf-8')
+                    if not name.endswith('$'):
+                        sAMAccountName = name
+                elif attr_type == 'pwdLastSet':
+                    val = int(str(attribute['vals'][0]))
+                    pwdLastSet = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
+                elif attr_type == 'lastLogon':
+                    val = int(str(attribute['vals'][0]))
+                    lastLogon = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
+                # elif attr_type == 'mail':
+                #     mail = str(attribute['vals'][0])
+                elif attr_type == 'whenCreated':
+                    val = str(attribute['vals'][0])
+                    whenCreated = str(self._changeGeneralizedTime(val))
+                   # description = str(attribute['vals'][0])
+                elif attr_type == 'description':
+                    description = str(attribute['vals'][0])
+
+            if sAMAccountName:
+                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
+                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
+                print(self.outputFormat.format(sAMAccountName, pwdLastSet, lastLogon, whenCreated))
+
+        except Exception as e:
+            logging.debug("Exception", exc_info=True)
+            logging.error(f"Error processing record: {e}")
+
+    def _draw_table(self, headers) -> None:
+        print(tabulate(self.rows, headers=headers, tablefmt="psql"))
+        # we clear the rows for the next execution
+        self.rows.clear()
+
+    def processDescription(self, item):
+        if not isinstance(item, SearchResultEntry):
+            return
+
+        sAMAccountName = ''
+        description = None
+
+        if self.debug: print("processDescription", item['attributes'])
+
+        try:
+            for attribute in item['attributes']:
+                attr_type = str(attribute['type'])
+                if attr_type == 'sAMAccountName':
+                    name = attribute['vals'][0].asOctets().decode('utf-8')
+                    if not name.endswith('$'):
+                        sAMAccountName = name
+                elif attr_type == 'description':
+                    # val = 
+                    description = str(attribute['vals'][0])
+
+            if sAMAccountName and description:
+                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
+                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
+                #print(self.attributes_list[0])
+                #print((sAMAccountName, description))
+                self.rows.append([sAMAccountName, description])
+
+        except Exception as e:
+            logging.debug("Exception", exc_info=True)
+            logging.error(f"Error processing record: {e}")
+
+    def processLastLogon(self, item):
+        if not isinstance(item, SearchResultEntry):
+            return
+
+        sAMAccountName = ''
+        lastLogon = None
+
+        if self.debug: print("processLastLogon", item['attributes'])
+
+        try:
+            for attribute in item['attributes']:
+                attr_type = str(attribute['type'])
+                if attr_type == 'sAMAccountName':
+                    name = attribute['vals'][0].asOctets().decode('utf-8')
+                    if not name.endswith('$'):
+                        sAMAccountName = name
+                elif attr_type == 'lastLogon':
+                    val = int(str(attribute['vals'][0]))
+                    lastLogon = "<never>" if val == 0 else str(datetime.fromtimestamp(self._getUnixTime(val)))
+
+            if sAMAccountName and lastLogon == "<never>":
+                # ["sAMAccountName", "description", "lastLogon", "userAccountControl","whenCreated"]
+                # print(self.outputFormat.format(sAMAccountName, description, mail, pwdLastSet, lastLogon))
+                #print((sAMAccountName, lastLogon))
+                self.rows.append([sAMAccountName, lastLogon])
+
+        except Exception as e:
+            logging.debug("Exception", exc_info=True)
+            logging.error(f"Error processing record: {e}")
+
+    def processWhenCreated(self, item):
+        if not isinstance(item, SearchResultEntry):
+            return
+
+        sAMAccountName = ''
+        lastLogon = None
+
+        if self.debug: print("processWhenCreated", item['attributes'])
+
+        try:
+            for attribute in item['attributes']:
+                attr_type = str(attribute['type'])
+                if attr_type == 'sAMAccountName':
+                    name = attribute['vals'][0].asOctets().decode('utf-8')
+                    if not name.endswith('$'):
+                        sAMAccountName = name
+                elif attr_type == 'whenCreated':
+                    val = str(attribute['vals'][0])
+                    whenCreated = str(self._changeGeneralizedTime(val))
+
+            if sAMAccountName and whenCreated:
+                #print((sAMAccountName, whenCreated))
+                self.rows.append([sAMAccountName, whenCreated])
+
+        except Exception as e:
+            logging.debug("Exception", exc_info=True)
+            logging.error(f"Error processing record: {e}")
+
+    def fetch_non_empty_descriptions(self, ldap_conn, search_filter):
+        print("Showing users' description if there's any passwords, care.")
+        sc = ldap.SimplePagedResultsControl(size=100)
+        ldap_conn.search(searchFilter=search_filter,
+                            attributes=self.attributes_list[0],
+                            searchControls=[sc],
+                            perRecordCallback=self.processDescription)
+        self._draw_table(self.attributes_list[0])
+           
+    def never_logged(self, ldap_conn, search_filter):
+        print("Showing users that have no logon date")
+        sc = ldap.SimplePagedResultsControl(size=100)
+        ldap_conn.search(searchFilter=search_filter,
+                             attributes=self.attributes_list[1],
+                             searchControls=[sc],
+                             perRecordCallback=self.processLastLogon)
+        self._draw_table(self.attributes_list[1])
+
+    def recent_users(self, ldap_conn, search_filter):
+        print("Showing creation date for each user")
+        sc = ldap.SimplePagedResultsControl(size=100)
+        ldap_conn.search(searchFilter=search_filter,
+                             attributes=self.attributes_list[2],
+                             searchControls=[sc],
+                             perRecordCallback=self.processWhenCreated)
+        self._draw_table(self.attributes_list[2])
